f161a5ec897ebf2e59c61561fd1699adf45f028e
Marko Petric <markop20042004@gmail.com>
2026-09-06 20:54:54 +0200

    Add ResetSkillDash cheat
    This cheat disables and enables SkillDash GFP, effectively resetting it. Mostly useful for debugging or as a fallback when something breaks during playtesting.

--- Source/SkillDashRuntime/Private/SdCheatExtension.cpp
+++ Source/SkillDashRuntime/Private/SdCheatExtension.cpp
@@ -3,7 +3,18 @@
 
 #include "SdCheatExtension.h"
 
+// Bomber
+#include "GfpmUtils.h"
+
 #include UE_INLINE_GENERATED_CPP_BY_NAME(SdCheatExtension)
+
+// Disables and enables the SkillDash GFP, effectively resetting it
+void USdCheatExtension::ResetSkillDash()
+{
+	const TArray<FName> SkillDash = {TEXT("SkillDash")};
+	UGfpmUtils::SetGameFeaturePluginsActive(false, SkillDash);
+	UGfpmUtils::SetGameFeaturePluginsActive(true, SkillDash);
+}
 
 // Override the impulse strength of the dash ability
 TAutoConsoleVariable<float> USdCheatExtension::CVarDashImpulseStrength(
--- Source/SkillDashRuntime/Public/SdCheatExtension.h
+++ Source/SkillDashRuntime/Public/SdCheatExtension.h
@@ -15,9 +15,13 @@ class SKILLDASHRUNTIME_API USdCheatExtension : public UMetaCheatManagerExtension
 	GENERATED_BODY()
 	
 public:
-	/** Override the impulse strength of the dash ability, where 0 is minimum */
+	/** Disables and enables the SkillDash GFP, effectively resetting it. */
+	UFUNCTION(Exec, meta = (CheatName = "ResetSkillDash"))
+	void ResetSkillDash();
+	
+	/** Override the impulse strength of the dash ability, where 0 is minimum. */
 	static TAutoConsoleVariable<float> CVarDashImpulseStrength;
 	
-	/** Override the cooldown of the dash ability, where 0.01 is the minimum (automatically clamped to 0.01 if 0 is put in) */
+	/** Override the cooldown of the dash ability, where 0.01 is the minimum (automatically clamped to 0.01 if 0 is put in). */
 	static TAutoConsoleVariable<float> CVarDashCooldownDuration;
 };
