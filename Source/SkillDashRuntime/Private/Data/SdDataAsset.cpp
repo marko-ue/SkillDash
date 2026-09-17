05ff2f6265031db4462231b5f11d12d0c2113db4
Marko Petric <markop20042004@gmail.com>
2026-09-17 08:40:47 +0200

    Improve comments for getters in SdDataAsset, properly capitalize local variables in getters

--- Source/SkillDashRuntime/Private/Data/SdDataAsset.cpp
+++ Source/SkillDashRuntime/Private/Data/SdDataAsset.cpp
@@ -16,30 +16,30 @@ const USdDataAsset& USdDataAsset::Get()
 	return UDalSubsystem::GetDataAssetChecked<ThisClass>();
 }
 
-// Returns default value from the data asset of the dash impulse strength
+// Returns impulse strength from the CVar if set, otherwise default value from data asset
 float USdDataAsset::GetDashImpulseStrength() const
 {
 #if !UE_BUILD_SHIPPING
-	const float CvarImpulseStrength = USdCheatExtension::CVarDashImpulseStrength.GetValueOnAnyThread();
-	if (CvarImpulseStrength >= 0.f)
+	const float CVarImpulseStrength = USdCheatExtension::CVarDashImpulseStrength.GetValueOnAnyThread();
+	if (CVarImpulseStrength >= 0.f)
 	{
-		return CvarImpulseStrength;
+		return CVarImpulseStrength;
 	}
 #endif // !UE_BUILD_SHIPPING
 
 	return DashImpulseStrength;
 }
 
-// Returns default value from the data asset of the dash cooldown duration
+// Returns cooldown duration from the CVar if set, otherwise default value from data asset
 float USdDataAsset::GetDashCooldownDuration() const
 {
 #if !UE_BUILD_SHIPPING
-	const float CvarCooldownDuration = USdCheatExtension::CVarDashCooldownDuration.GetValueOnAnyThread();
-	if (CvarCooldownDuration >= 0.f)
+	const float CVarCooldownDuration = USdCheatExtension::CVarDashCooldownDuration.GetValueOnAnyThread();
+	if (CVarCooldownDuration >= 0.f)
 	{
 		// Always clamps to a minimum of 0.01, which prevents the cooldown duration from ever being set to 0,
 		// because a cooldown of 0 makes the cooldown stay forever
-		return FMath::Max(KINDA_SMALL_NUMBER, CvarCooldownDuration);
+		return FMath::Max(KINDA_SMALL_NUMBER, CVarCooldownDuration);
 	}
 #endif // !UE_BUILD_SHIPPING
 
